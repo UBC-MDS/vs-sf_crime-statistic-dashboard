0d92a2d2ea106168b9fb675b2c30c0081389c3b4
Lesley <iamthelm@gmail.com>
2019-12-12 12:15:37 -0800

    added css styling to app layout

--- static_plots.R
+++ static_plots.R
@@ -15,14 +15,15 @@ top4_df <- df %>%
              Category == 'VEHICLE THEFT')
 
 # create a frequency table for the crimes in each neighborhood
-neighborhood_freq <- as.data.frame(table(top4_df$PdDistrict))
+neighborhood_freq <- as.data.frame(table(top4_df$PdDistrict)) %>% 
+   mutate(Var1 = fct_reorder(as.factor(Var1), Freq, .desc = TRUE))
 colnames(neighborhood_freq) <- c('neighborhood', 'Freq')
 
 
 # make the frequency chart for crime over neighborhood
 # note: static plot is counts of all 4 crimes together for each neighborhood
 bar_chart <- neighborhood_freq %>% 
-      ggplot(aes(x = reorder(neighborhood, -neighborhood_freq$Freq), y = Freq, fill = neighborhood)) + 
+      ggplot(aes(x = neighborhood, y = Freq, fill = neighborhood)) + 
       geom_col() +
       labs(title = "Distribution of Crime Reports Across Neighborhood",
            x = "Neighborhood",
@@ -39,7 +40,7 @@ bar_chart
 # make  map plot 
 # note: static plot is density for all top 4 crimes in each neighborhood
 
-map_plot <- top4_df %>% 
+map_plot <- top4_df %>% q
       ggplot(aes(x = X, y = Y, color = PdDistrict)) +
       geom_point(size = 0.07) +
       labs(title = 'Crime Density Across Neighborhoods',
